9f53d081b0f88d1d0e5946a9a75c006b0725edfc
besumero628 <besumero628@gmail.com>
2021-07-16 12:18:40 +0000

    [Add] 403 error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,12 @@
 class ApplicationController < ActionController::Base
   layout :set_layout
 
+  class Forbidden < ActionController::ActionControllerError; end
+  class IpAddressRejected < ActionController::ActionControllerError; end
+
   rescue_from StandardError, with: :rescue500
+  rescue_from Forbidden, with: :rescue403
+  rescue_from IpAddressRejected, with: :rescue403
 
   private
   def set_layout
@@ -14,5 +19,10 @@ class ApplicationController < ActionController::Base
 
   def rescue500(e)
     render "errors/internal_server_error", status: 500
+  end
+
+  def rescue403(e)
+    @exception = e
+    render "errors/forbidden", status: 403
   end
 end
