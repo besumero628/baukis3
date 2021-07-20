0b947d2f8cc41f89321f3ea44aff28f61d8b37f1
besumero628 <besumero628@gmail.com>
2021-07-20 14:55:27 +0000

    [Add] bad request error 400

--- app/controllers/concerns/error_handlers.rb
+++ app/controllers/concerns/error_handlers.rb
@@ -6,6 +6,7 @@ module ErrorHandlers
     rescue_from ApplicationController::Forbidden, with: :rescue403
     rescue_from ApplicationController::IpAddressRejected, with: :rescue403
     rescue_from ActiveRecord::RecordNotFound, with: :rescue404
+    rescue_from ActionController::ParameterMissing, with: :rescue400
   end
 
   private
@@ -20,6 +21,10 @@ module ErrorHandlers
 
   def rescue404(e)
     render "errors/not_found", status: 404
+  end
+
+  def rescue400(e)
+    render "errors/bad_request", status: 400
   end
   
 end
