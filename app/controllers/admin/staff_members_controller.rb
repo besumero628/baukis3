ec86b0e83c426d72f3e0e26a994dd0331da49ada
besumero628 <besumero628@gmail.com>
2021-07-21 05:13:02 +0000

    [Update] admin authorize method

--- app/controllers/admin/staff_members_controller.rb
+++ app/controllers/admin/staff_members_controller.rb
@@ -1,4 +1,6 @@
 class Admin::StaffMembersController < Admin::Base
+
+
   def index
     @staff_members = StaffMember.order(:family_name_kana, :given_name_kana)
   end
@@ -45,6 +47,7 @@ class Admin::StaffMembersController < Admin::Base
   end
 
   private
+
   def staff_member_params
     params.require(:staff_member).permit(
       :email, :password, :family_name, :given_name,
